63634f72501635df4beecdff629b446baf90f233
SedatTurkoglu16 <sedatturkoglu16@gmail.com>
2022-02-09 17:15:27 +0300

    context was created and setted up

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import { Fragment, useState } from 'react';
+import { useState } from 'react';
 import './App.css';
 import Cart from './components/Cart/Cart';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
+import CartProvider from './store/CartProvider';
 
 
 function App() {
@@ -17,13 +18,13 @@ function App() {
   }
 
   return (
-    <Fragment>
+    <CartProvider>
         {cartIsShown && <Cart onClose={hideCartHandler}/>}
         <Header onShowCart={showCartHandler}/>
         <main>
           <Meals />
         </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
